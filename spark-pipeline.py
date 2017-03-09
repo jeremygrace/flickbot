33a2e06355cf6b1993b13d4beeafb47a02020de7
Jeremy Grace <jeremy.grace@outlook.com>
2017-03-09 04:12:04 -0800

    Update spark pipeline code

--- spark-pipeline.py
+++ spark-pipeline.py
@@ -2,6 +2,26 @@
 from pyspark import SparkConf, SparkContext
 
 
+def deploy_spark():
+    pass
+
+
+def load_dataframes():
+    pass
+
+
+def perform_joins():
+    pass
+
+
+def send_parquets():
+    pass
+
+
+def send_to_RDS():
+    pass
+
+
 if __name__ == '__main__':
     conf = SparkConf() \
         .setAppName('Flickbot') \
